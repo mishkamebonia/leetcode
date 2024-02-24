c35b27762e31913f70a7b640687338f3264ddcab
mishkamebonia <mishkas.mebonia@gmail.com>
2024-02-24 21:56:38 +0400

    longest common prefix

---
+++ longest-common-prefix.js
@@ -0,0 +1,20 @@
+strs = ["flower", "flow", "flight"];
+
+var longestCommonPrefix = function (strs) {
+  let preset = "";
+  let firstEl = strs[0];
+
+  for (let i = 0; i < firstEl.length; i++) {
+    for (let j = strs.length - 1; j < strs.length; j++) {
+      if (firstEl[i] === strs[j][i]) {
+        preset += firstEl[i];
+      } else {
+        return preset;
+      }
+    }
+  }
+
+  return preset;
+};
+
+console.log(longestCommonPrefix(strs));
